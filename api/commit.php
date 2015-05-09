bd4c6a221757ad730d32da55b292114f28813012
lwch <lwch748@gmail.com>
2015-05-09 22:02:59 +0800

    disable ip from 10.0.0.0/8

--- api/commit.php
+++ api/commit.php
@@ -3,6 +3,9 @@ require __DIR__.'/common.php';
 
 global $allow;
 $redis = redis();
+$ip = ip2long($_SERVER['REMOTE_ADDR']);
+if (($ip >> 24) & 0xFF == 10)
+    exit;
 if (in_array($_REQUEST['id'], $allow) and $redis->get('us~'.$_SERVER['REMOTE_ADDR']) === null) {
     $pipe = $redis->pipeline();
     $pipe->incr($_REQUEST['id']);
