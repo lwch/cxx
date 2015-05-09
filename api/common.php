d5a4327bb12da9fbe5160c750c2749c0cde0a355
lwch <lwch748@gmail.com>
2015-05-09 22:58:57 +0800

    add more languages

--- api/common.php
+++ api/common.php
@@ -16,5 +16,5 @@ function redis() {
 }
 
 $allow = array(
-    'cxx', 'php', 'python', 'shanghai', 'yueyu', 'putong', 'other'
+    'asm', 'c', 'cxx', 'csharp', 'java', 'lua', 'php', 'python', 'shanghai', 'yueyu', 'putong', 'other'
 );
