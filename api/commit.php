de9099ecb8c99763a58ed8e046ac31312e6457d7
lwch <lwch748@gmail.com>
2015-05-10 15:18:02 +0800

    1. enable ip from 10.0.0.0/8 2. change time limit to 1 hour

--- api/commit.php
+++ api/commit.php
@@ -2,15 +2,11 @@
 require __DIR__.'/common.php';
 
 global $allow;
-if (substr($_SERVER['REMOTE_ADDR'], 0, 3) == '10.') {
-    echo $_REQUEST['callback'], '({"stat":1});';
-    exit;
-}
 $redis = redis();
 if (in_array($_REQUEST['id'], $allow) and $redis->get('us~'.$_SERVER['REMOTE_ADDR']) === null) {
     $pipe = $redis->pipeline();
     $pipe->incr($_REQUEST['id']);
-    $pipe->setex('us~'.$_SERVER['REMOTE_ADDR'], 1, time());
+    $pipe->setex('us~'.$_SERVER['REMOTE_ADDR'], 3600, time());
     $pipe->execute();
     echo $_REQUEST['callback'], '({"stat":0});';
     exit;
