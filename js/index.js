13a729ba5be54f369c6c15c4acaf7b865522b68b
lwch <lwch748@gmail.com>
2015-05-10 21:52:14 +0800

    supported cartogram

--- js/index.js
+++ js/index.js
@@ -3,14 +3,18 @@ $(function() {
         $this = $(this);
         page.commit($this.attr('id'));
     });
-    $('#refresh').click(function() {
-        page.update();
-    });
-    $('#report').click(function() {
-        report.show();
-    });
+    $('#refresh').click(page.update);
+    $('#report').click(report.show);
+    $('#cartogram').click(cartogram.show);
     page.update();
 });
+
+var common = {
+    id2show: function(id) {
+        console.log($('#group>.row>div>p>#'+id+'>#name').text());
+        return $('#group>.row>div>p>#'+id+'>#name').text();
+    }
+};
 
 var page = {
     commit: function(id) {
@@ -32,7 +36,7 @@ var page = {
             dataType: 'jsonp'
         }).success(function(data) {
             $.each(data, function(k, v) {
-                $('#group>.row>div>p>#'+k+'>span').text(v);
+                $('#group>.row>div>p>#'+k+'>#cnt').text(v);
             });
         });
     }
@@ -52,6 +56,53 @@ var report = {
             });
             $('#md-report table>tbody').append($('<tr><th>总数</th><th>'+data.total+'</th></tr>'));
             $('#md-report').modal();
+        });
+    }
+};
+
+var cartogram = {
+    show: function() {
+        $.ajax({
+            url: 'http://cxx.coding.io/api/fetch.php',
+            method: 'GET',
+            cache: false,
+            dataType: 'jsonp'
+        }).success(function(data) {
+            var input = [];
+            var i = 0;
+            $.each(data, function(k, v) {
+                input[i++] = [common.id2show(k), parseInt(v)];
+            });
+            console.log(input);
+            $('#md-cartogram #container').highcharts({
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false
+                },
+                title: {
+                    text: '统计图'
+                },
+                tooltip: {
+                    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: false
+                        },
+                        showInLegend: true
+                    }
+                },
+                series: [{
+                    type: 'pie',
+                    name: '人数：',
+                    data: input
+                }]
+            });
+            $('#md-cartogram').modal();
         });
     }
 };
