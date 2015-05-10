5f71597b042cca6e85771e3b3bfc93658ce00bae
lwch <lwch748@gmail.com>
2015-05-10 16:29:26 +0800

    change info api for old version redis support

--- api/info.php
+++ api/info.php
@@ -1,17 +1,25 @@
 <?php
 require __DIR__.'/common.php';
-define('LIMIT', 1000);
+define('LIMIT', 100);
 
 $redis = redis();
-$cur = 0;
-$ret = array('total' => 0);
-do {
-    $scan = $redis->scan($cur, array('MATCH' => 'us~*', 'COUNT' => LIMIT));
-    $ret['total'] += count($scan[1]);
-    foreach ($redis->mget($scan[1]) as $addr) {
+$keys = $redis->keys('us~*');
+$ret = array('total' => count($keys), 'data' => array());
+$tmp = array();
+while (count(keys)) {
+    $tmp[] = array_pop($keys);
+    if (count($tmp) >= LIMIT) {
+        foreach ($redis->mget($tmp) as $addr) {
+            if (!isset($ret['data'][$addr])) $ret['data'][$addr] = 0;
+            ++$ret['data'][$addr];
+        }
+    }
+}
+if (count($tmp)) {
+    foreach ($redis->mget($tmp) as $addr) {
         if (!isset($ret['data'][$addr])) $ret['data'][$addr] = 0;
         ++$ret['data'][$addr];
     }
-    $cur = $scan[0];
-} while ($cur);
+}
+ksort($ret['data']);
 echo $_REQUEST['callback'], '(', json_encode($ret), ');';
