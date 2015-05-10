24ef6bec1123c0c6dd5f85336e6fc4b6b965e000
lwch <lwch748@gmail.com>
2015-05-10 17:19:55 +0800

    change info api

--- api/info.php
+++ api/info.php
@@ -6,23 +6,17 @@ $redis = redis();
 $keys = $redis->keys('us~*');
 $ret = array('total' => count($keys), 'data' => array());
 $pipe = $redis->pipeline();
-$c = 0;
-while (count(keys)) {
-    $pipe->get(array_pop($keys));
-    file_put_contents('abc', $c."\n", FILE_APPEND);
-    if (++$c % LIMIT == 0) {
-        foreach ($pipe->execute() as $addr) {
+if (count($keys) > LIMIT) {
+    foreach (array_chunk($keys, LIMIT) as $g) {
+        foreach ($redis->mget($g) as $addr) {
             if ($addr === null) continue;
             if (!isset($ret['data'][$addr])) $ret['data'][$addr] = 0;
             ++$ret['data'][$addr];
         }
-        $pipe = $redis->pipeline();
     }
-}
-echo $_REQUEST['callback'], '(', json_encode($ret), ');';exit;
-if (count($tmp)) {
-    $r = call_user_func_array(array($redis, 'mget'), $tmp);
-    foreach ($r as $addr) {
+} else {
+    foreach ($redis->mget($keys) as $addr) {
+        if ($addr === null) continue;
         if (!isset($ret['data'][$addr])) $ret['data'][$addr] = 0;
         ++$ret['data'][$addr];
     }
