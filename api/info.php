ca68148472348101a46738336635efec7ff27306
lwch <lwch748@gmail.com>
2015-05-10 16:44:17 +0800

    debug

--- api/info.php
+++ api/info.php
@@ -5,7 +5,6 @@ define('LIMIT', 100);
 $redis = redis();
 $keys = $redis->keys('us~*');
 $ret = array('total' => count($keys), 'data' => array());
-echo $_REQUEST['callback'], '(', json_encode($ret), ');';exit;
 $tmp = array();
 while (count(keys)) {
     $tmp[] = array_pop($keys);
@@ -17,6 +16,7 @@ while (count(keys)) {
         $tmp = array();
     }
 }
+echo $_REQUEST['callback'], '(', json_encode($ret), ');';exit;
 if (count($tmp)) {
     foreach ($redis->mget($tmp) as $addr) {
         if (!isset($ret['data'][$addr])) $ret['data'][$addr] = 0;
