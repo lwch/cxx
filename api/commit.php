8a4b4d5b3ca36d604fba571fd72a686413e9dedc
lwch <lwch748@gmail.com>
2015-05-10 15:31:56 +0800

    change commit time from 1 seconds to 3600 seconds

--- api/commit.php
+++ api/commit.php
@@ -7,7 +7,7 @@ $ip = realip();
 if (in_array($_REQUEST['id'], $allow) and $redis->get('us~'.$ip) === null) {
     $pipe = $redis->pipeline();
     $pipe->incr($_REQUEST['id']);
-    $pipe->setex('us~'.$ip, 1, time());
+    $pipe->setex('us~'.$ip, 3600, time());
     $pipe->execute();
     echo $_REQUEST['callback'], '({"stat":0});';
     exit;
