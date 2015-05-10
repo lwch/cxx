91745e6bf2b29c9fcb7a3ff32e32772206b95c74
lwch <lwch748@gmail.com>
2015-05-10 17:45:51 +0800

    support report

--- js/index.js
+++ js/index.js
@@ -6,6 +6,9 @@ $(function() {
     $('#refresh').click(function() {
         page.update();
     });
+    $('#report').click(function() {
+        report.show();
+    });
     page.update();
 });
 
@@ -31,6 +34,24 @@ var page = {
             $.each(data, function(k, v) {
                 $('#group>.row>div>p>#'+k+'>span').text(v);
             });
+        });
+    }
+};
+
+var report = {
+    show: function() {
+        $.ajax({
+            url: 'http://cxx.coding.io/api/info.php',
+            method: 'GET',
+            cache: false,
+            dataType: 'jsonp'
+        }).success(function(data) {
+            $('#md-report table>tbody').empty();
+            $.each(data.data, function(k, v) {
+                $('#md-report table>tbody').append($('<tr><td>'+k+'</td><td>'+v+'</td></tr>'));
+            });
+            $('#md-report table>tbody').append($('<tr><th>总数</th><th>'+data.total+'</th></tr>'));
+            $('#md-report').modal();
         });
     }
 };
