ae71058cbe4f635a923a8441e2cb61c3e09b3180
lwch <lwch748@gmail.com>
2015-05-10 15:09:57 +0800

    move redis connection behind

--- api/commit.php
+++ api/commit.php
@@ -2,11 +2,11 @@
 require __DIR__.'/common.php';
 
 global $allow;
-$redis = redis();
 if (substr($_SERVER['REMOTE_ADDR'], 0, 3) == '10.') {
     echo $_REQUEST['callback'], '({"stat":1});';
     exit;
 }
+$redis = redis();
 if (in_array($_REQUEST['id'], $allow) and $redis->get('us~'.$_SERVER['REMOTE_ADDR']) === null) {
     $pipe = $redis->pipeline();
     $pipe->incr($_REQUEST['id']);
